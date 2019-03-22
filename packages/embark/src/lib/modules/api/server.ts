8d7808d47423ec739682620f8664f56e8af80f2c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-03-22 17:50:09 -0500

    .

--- packages/embark/src/lib/modules/api/server.ts
+++ packages/embark/src/lib/modules/api/server.ts
@@ -11,7 +11,7 @@ import {__} from "i18n";
 import * as path from "path";
 import * as ws from "ws";
 // @ts-ignore
-import { embarkPath } from "../../core/fs";
+import { embarkPath, existsSync } from "../../core/fs";
 
 type Method = "get" | "post" | "ws" | "delete";
 
@@ -101,22 +101,140 @@ export default class Server {
 
     this.embark.events.on("plugins:register:api", (callDescription: CallDescription) => this.registerCallDescription(instance, callDescription));
 
-    let buildDir = "";
-    let ui: express.RequestHandler;
-    if (process.env.EMBARK_DEVELOPMENT) {
-      ui = proxy("http://localhost:3000");
-    } else {
-      buildDir = findUp.sync("node_modules/embark-ui/build", {cwd: embarkPath()}) ||
-        embarkPath("node_modules/embark-ui/build");
-      ui = express.static(buildDir);
+    let monorepoRootDir = "";
+    const isInsideMonorepo = existsSync(path.join(embarkPath(), "../../packages/embark"));
+    if (isInsideMonorepo) {
+      monorepoRootDir = path.resolve(path.join(embarkPath(), "../.."));
     }
+    const buildDir = findUp.sync("node_modules/embark-ui/build", {cwd: embarkPath()}) ||
+      embarkPath("node_modules/embark-ui/build");
 
-    instance.app.use("/", ui);
-
-    if (buildDir) {
-      instance.app.get("/*", (_req, res) => {
-        res.sendFile(path.join(buildDir, "index.html"));
-      });
+    if (!isInsideMonorepo || process.env.EMBARK_UI_STATIC) {
+      if (existsSync(path.join(buildDir, "index.html"))) {
+        instance.app.use("/", express.static(buildDir));
+        instance.app.get("/*", (_req, res) => {
+          res.sendFile(path.join(buildDir, "index.html"));
+        });
+      } else {
+        let envReport = "";
+        let inside = `in <code>${path.dirname(buildDir)}</code>`;
+        let notice = "<p>this distribution of <code>embark-ui</code> appears to be broken</p>";
+        if (isInsideMonorepo) {
+          envReport = `<p><code>process.env.EMBARK_UI_STATIC=${process.env.EMBARK_UI_STATIC}</code></p>`;
+          inside = `inside the monorepo at <code>${path.join(monorepoRootDir, "packages/embark-ui")}</code>`;
+          notice = `
+            <p>to build <code>embark-ui</code> please run either:</p>
+            <p><code>cd ${monorepoRootDir} && yarn build</code><br />
+              or<br />
+              <code>cd ${path.join(monorepoRootDir, "packages/embark-ui")}
+                && yarn build</code></p>
+            <p>restart <code>embark run</code> after building <code>embark-ui</code></p>
+          `;
+        }
+        const page = (reloadSec: number) => (`
+          <!doctype html>
+          <html lang="en">
+            <head>
+              <meta charset="utf-8" />
+              ${isInsideMonorepo ? `<meta http-equiv="refresh" content="${reloadSec}">` : ""}
+              <title>Embark API Server</title>
+              <style type="text/css">
+                code {
+                  background-color: rgba(220,220,220,.5);
+                }
+              </style>
+            </head>
+            <body>
+              ${envReport}
+              <p>missing build for package <code>embark-ui</code> ${inside}</p>
+              ${notice}
+              ${isInsideMonorepo ? `
+                <p>this page will automatically reload
+                  <span id="time-left">
+                    in ${reloadSec} second${reloadSec !== 1 ? "s" : ""}
+                  </span></p>
+                <script>
+                  let timeLeft = ${reloadSec};
+                  const span = document.querySelector("#time-left");
+                  setInterval(() => {
+                    if (timeLeft >= 1) { timeLeft -= 1; }
+                    if (!timeLeft) { return span.innerText = "now"; }
+                    span.innerText = \`in \${timeLeft} second\${timeLeft !== 1 ? "s" : ""}\`;
+                  }, 1000);
+                </script>
+              ` : ""}
+            </body>
+          </html>
+        `.trim().split("\n").map((str) => str.trim()).filter((str) => str).join("\n"));
+        const page404 = page(3);
+        const missingBuildHandler = (_req: Request, res: Response) => {
+          res.status(404).send(page404);
+        };
+        instance.app.get("/", missingBuildHandler);
+        instance.app.get("/*", missingBuildHandler);
+      }
+    } else {
+      const pageEConnError = (waitingFor: string, reloadSec: number) => (`
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <meta http-equiv="refresh" content="${reloadSec}">
+            <title>Embark API Server</title>
+            <style type="text/css">
+              code {
+                background-color: rgba(220,220,220,.5);
+              }
+            </style>
+          </head>
+          <body>
+            <p><code>lib/modules/api/server</code> inside the monorepo at
+              <code>${path.join(monorepoRootDir, "embark")}</code> is waiting
+              for the Create React App development server of package
+              <code>embark-ui</code> to ${waitingFor} at
+              <code>localhost:55555</code></p>
+            ${waitingFor === "become available" ? `
+              <p>please run either:</p>
+              <p><code>cd ${monorepoRootDir} && yarn start</code><br />
+                or<br />
+                <code>cd ${path.join(monorepoRootDir, "packages/embark-ui")}
+                  && yarn start</code></p>
+            ` : ""}
+            <p>this page will automatically reload
+               <span id="time-left">
+                 in ${reloadSec} second${reloadSec !== 1 ? "s" : ""}
+               </span></p>
+            <script>
+              let timeLeft = ${reloadSec};
+              const span = document.querySelector("#time-left");
+              setInterval(() => {
+                if (timeLeft >= 1) { timeLeft -= 1; }
+                if (!timeLeft) { return span.innerText = "now"; }
+                span.innerText = \`in \${timeLeft} second\${timeLeft !== 1 ? "s" : ""}\`;
+              }, 1000);
+            </script>
+          </body>
+        </html>
+      `.trim().split("\n").map((str) => str.trim()).filter((str) => str).join("\n"));
+      const pageEconnRefused = pageEConnError("become available", 3);
+      const pageEconnReset = pageEConnError("become responsive", 3);
+      instance.app.use("/", proxy("http://localhost:3000", {
+        // @ts-ignore
+        proxyErrorHandler: (err, res, next) => {
+          switch (err && err.code) {
+            case "ECONNREFUSED": {
+              return res.status(503).send(pageEconnRefused);
+            }
+            case "ECONNRESET": {
+              if (err.message === "socket hang up") {
+                return res.status(504).send(pageEconnReset);
+              }
+            }
+            default: { next(err); }
+          }
+        },
+        timeout: 1000,
+      }));
     }
 
     return instance;
